5dbd124820d13e35befbc952a90c74205a15b755
GeeKaven <i@tawawa.moe>
2022-04-04 17:29:51 +0800

    feat: support cil argv

--- vol.js
+++ vol.js
@@ -3,6 +3,7 @@ import { parse, extname, basename } from 'path'
 import StreamZip from 'node-stream-zip'
 import archiver from 'archiver'
 import cheerio from 'cheerio'
+import minimist from 'minimist'
 
 // 文件重命名
 async function suffixChange(inType, outType, path) {
@@ -12,11 +13,12 @@ async function suffixChange(inType, outType, path) {
             let fileSeq = parse(file);
             if (fileSeq.ext === inType) {
                 let newFile = fileSeq.name + outType;
+                let oldPath = path + '/' + file;
                 let newPath = path + '/' + newFile;
-                await rename(file, newPath);
+                await rename(oldPath, newPath);
             }
         }
-          
+
     } catch (err) {
         console.error(err)
     }
@@ -25,18 +27,19 @@ async function suffixChange(inType, outType, path) {
 // 单个压缩包根据 html 文件中的图片地址进行提取
 async function loadZipImg(zipFile, cacheFolder) {
 
+    console.log(`开始解析 ${zipFile}`)
     const comicName = zipFile.match(/moe](.+?)\.+?/)[1];
     console.log(`${comicName} => 开始提取 `)
     const comicCacheFolder = cacheFolder + '/' + comicName;
     await mkdir(comicCacheFolder, { recursive: true });
 
-    const zip = new StreamZip.async({ file : zipFile })
+    const zip = new StreamZip.async({ file: zipFile })
     const entries = await zip.entries()
-    
+
     const re = /^\d{1,3}.html$/
 
     for (const entry of Object.values(entries)) {
-        const entryName =  basename(entry.name)
+        const entryName = basename(entry.name)
         if (entryName === 'cover.jpg' || entryName === 'cover.png') {
             await zip.extract(entry.name, comicCacheFolder + '/' + entryName)
         }
@@ -44,7 +47,7 @@ async function loadZipImg(zipFile, cacheFolder) {
             const $ = cheerio.load(await zip.entryData(entry.name), { decodeEntities: false })
             const imgPath = $('.fs img').attr('src').slice(3)
             const title = $('title').text()
-            await zip.extract(imgPath, comicCacheFolder + '/' + title + extname(imgPath))            
+            await zip.extract(imgPath, comicCacheFolder + '/' + title + extname(imgPath))
         }
     }
 
@@ -67,7 +70,7 @@ async function classfyComic(path, cacheFolder) {
         for (const file of files) {
             let fileExt = extname(file);
             if (fileExt === '.zip') {
-                await loadZipImg(file, cacheFolder)
+                await loadZipImg(path + '/' + file, cacheFolder)
             }
         }
     } catch (error) {
@@ -82,7 +85,7 @@ async function packFolder(comicFolder, cacheFolder, outputFolder) {
         const output = outputFd.createWriteStream();
 
         const archive = archiver('zip', {
-            zlib: { level: 9 } 
+            zlib: { level: 9 }
         });
 
         archive.pipe(output)
@@ -117,19 +120,29 @@ async function packComic(cacheFolder, outputFolder) {
     }
 }
 
-let path = '.'
-const cacheFolder = path + '/cache'
-const outputFolder = path + '/output'
+const argv = minimist(process.argv.slice(2));
 
-console.log('============> 开始重命名文件')
-await suffixChange('.epub','.zip', path)
+let path = argv['path'] || ''
+console.log(path)
 
-console.log('============> 开始提取图片')
-await classfyComic(path, cacheFolder)
+if (path) {
+    const cacheFolder = path + '/cache'
+    const outputFolder = path + '/output'
 
-console.log('============> 开始打包')
-await packComic(cacheFolder, outputFolder)
+    console.log('============> 开始重命名文件')
+    await suffixChange('.epub', '.zip', path)
 
-console.log('============> 开始清理缓存文件')
-await rm(cacheFolder, { recursive: true })
-console.log('============> 完成')
+    console.log('============> 开始提取图片')
+    await classfyComic(path, cacheFolder)
+
+    console.log('============> 开始打包')
+    await packComic(cacheFolder, outputFolder)
+
+    console.log('============> 开始清理缓存文件')
+    await rm(cacheFolder, { recursive: true })
+    console.log('============> 完成')
+} else {
+    console.log('请输入漫画 epub 文件所在文件夹路径')
+}
+
+// TODO: 增加多线程支持
